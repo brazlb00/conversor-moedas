7fdf5b6815addd955bdeee99273ffec6234c096e
brazlb00 <brazlb00@outlook.com>
2023-05-02 16:06:17 -0300

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django.heroku.settings(locals())
+django_heroku.settings(locals())
 
